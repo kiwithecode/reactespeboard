826e21c691e41a03f0cddd52beee9c9c47f08f6c
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-08-19 13:19:57 -0500

    feature(profesorController): funcionalidades de registro y login

--- controllers/ProfesorController.js
+++ controllers/ProfesorController.js
@@ -1,31 +1,76 @@
-// controllers/profesorController.js
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 const Profesor = require('../models/modelProfesores');
 const RegistroFirmas = require('../models/modelRegistroFirmasFirmas');
 
 // Middleware para cargar la firma electrónica del profesor
 exports.cargarFirmaElectronica = async (req, res) => {
-  const profesorId = req.user.id; // Suponiendo que tienes la información del profesor desde la autenticación (passport o similar)
-  const firmaElectronica = req.file.buffer; // Obtiene el objeto Buffer del archivo cargado
+  const profesorId = req.user.id;
+  const firmaElectronica = req.file.buffer;
 
-  // Actualiza la firma electrónica del profesor en la base de datos
   await Profesor.findByIdAndUpdate(profesorId, { firmaElectronica });
 
-  res.redirect('/dashboard'); // Redirige a la página de inicio del profesor
+  res.redirect('/dashboard');
 };
 
 // Middleware para registrar la firma en una clase
 exports.registrarFirma = async (req, res) => {
-  const profesorId = req.user.id; // Suponiendo que tienes la información del profesor desde la autenticación (passport o similar)
-  const claseId = req.body.claseId; // Suponiendo que obtienes el ID de la clase desde el formulario
+  const profesorId = req.user.id;
+  const claseId = req.body.claseId;
 
-  // Verifica si el profesor ha cargado su firma electrónica
   const profesor = await Profesor.findById(profesorId);
   if (!profesor.firmaElectronica) {
     return res.render('cargarFirma', { error: 'Debes cargar tu firma electrónica antes de registrar la firma en la clase.' });
   }
 
-  // Guarda el registro de firma en la base de datos
   await RegistroFirmas.create({ profesor_id: profesorId, clase_id: claseId, fecha_registro: new Date() });
 
-  res.redirect('/dashboard'); // Redirige a la página de inicio del profesor con un mensaje de éxito
+  res.redirect('/dashboard');
+};
+
+// Función para registrar un profesor
+exports.register = async (req, res) => {
+    try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+        
+        const newProfesor = new Profesor({
+            nombre: req.body.nombre,
+            ID: req.body.ID,
+            cedula: req.body.cedula,
+            fecha_nacimiento: req.body.fecha_nacimiento,
+            correo: req.body.correo,
+            password: hashedPassword
+        });
+
+        const savedProfesor = await newProfesor.save();
+
+        res.status(200).send({ message: 'Registration successful', profesor: savedProfesor });
+    } catch (error) {
+        res.status(500).send({ message: 'Error registering the professor.' });
+    }
+};
+
+// Función para iniciar sesión como profesor
+exports.login = async (req, res) => {
+    try {
+        const profesor = await Profesor.findOne({ correo: req.body.correo });
+        if (!profesor) {
+            return res.status(401).send({ message: 'Authentication failed. Professor not found.' });
+        }
+
+        profesor.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch || err) {
+                return res.status(401).send({ message: 'Authentication failed. Wrong password.' });
+            }
+
+            const token = jwt.sign({ id: profesor._id }, 'YOUR_SECRET_STRING', {
+                expiresIn: 86400
+            });
+
+            res.status(200).send({ auth: true, token: token });
+        });
+
+    } catch (error) {
+        res.status(500).send({ message: 'Error on the server.' });
+    }
 };
