6f2368622f8a20ebaa20b15129e563ead9e67148
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-08-19 12:43:21 -0500

    feature(modelProfesores): se inserto una funcionalidad para el login y autenticacion de usuarios

--- models/modelProfesores.js
+++ models/modelProfesores.js
@@ -1,21 +1,41 @@
 const mongoose = require('mongoose');
-
-//Esquema
+const bcrypt = require('bcryptjs');  // hash password
+// Esquema
 const { Schema } = mongoose;
 
 const ProfesoresSchema = new Schema({
-    nombre: { type: String, required: true },
-    apellido: { type: String, required: true },
+    nombre: { type: String, required: true, unique: true },
+    ID: { type: String, required: true, unique: true },
+    cedula: { type: String, required: true, unique: true },
+    fecha_nacimiento: { type: Date, required: true },
     correo: { type: String, required: true, unique: true },
-    telefono: { type: String, required: true, unique: true },
-    departamento: { type: String, required: true },
-    firmaElectronica: { type: Buffer } // Nuevo campo para la firma electrónica del profesor en formato binario
+    firmaElectronica: { type: Buffer }, // Nuevo campo para la firma electrónica del profesor en formato binario
 
+    password: { type: String, required: true }
 }, {
-    timestamps: true,
-    versionKey: false
+    timestamps: true
 });
-//Modelo
+
+// antes de guardar hacer hash al password
+ProfesoresSchema.pre('save', function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const salt = bcrypt.genSaltSync(10);
+        this.password = bcrypt.hashSync(this.password, salt);
+    }
+    next();
+});
+
+// metodo para comprar passwords (login)
+ProfesoresSchema.methods.comparePassword = function(passw, cb) {
+    bcrypt.compare(passw, this.password, (err, isMatch) => {
+        if (err) {
+            return cb(err);
+        }
+        cb(null, isMatch);
+    });
+};
+
+// Modelo
 const Profesores = mongoose.model('profesores', ProfesoresSchema);
 
-module.exports = { Profesores}
+module.exports = { Profesores }
