5bc801ffca7389f20a59f42eb2c66384a069f0f1
Pablo Guevara <pablo_alexa22@hotmail.com>
2023-08-19 13:10:21 -0500

    feature(authmiddleware): Middleware para la autenticacion

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,21 @@
+const jwt = require('jsonwebtoken');
+
+function verifyToken(req, res, next) {
+    const token = req.headers['x-access-token'];
+
+    if (!token) {
+        return res.status(403).send({ auth: false, message: 'No token provided.' });
+    }
+
+    jwt.verify(token, 'YOUR_SECRET_STRING', (err, decoded) => {
+        if (err) {
+            return res.status(500).send({ auth: false, message: 'Failed to authenticate token.' });
+        }
+
+        // If token is verified, set the user's ID for the request
+        req.userId = decoded.id;
+        next();
+    });
+}
+
+module.exports = verifyToken;
